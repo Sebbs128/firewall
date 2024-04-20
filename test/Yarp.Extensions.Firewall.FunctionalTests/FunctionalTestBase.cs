5f4cfdecc56336383c64e73e7bc84e02010389bb
Wayne Sebbens <sebbo_oz@hotmail.com>
2024-04-20 10:43:29 +1000

    Address warnings in test projects about use of nullable reference types wtihout a nullable context

--- test/Yarp.Extensions.Firewall.FunctionalTests/FunctionalTestBase.cs
+++ test/Yarp.Extensions.Firewall.FunctionalTests/FunctionalTestBase.cs
@@ -1,16 +1,14 @@
 using System.Net.Http.Headers;
 
-using Yarp.Extensions.Firewall.FunctionalTests.Common;
-
 namespace Yarp.Extensions.Firewall.FunctionalTests;
 
 public abstract class FunctionalTestBase
 {
     public virtual async Task<HttpResponseMessage> SendHttpRequest(
     Uri requestUri,
-    HttpMethod? httpMethod = default,
-    HttpContent? content = default,
-    IDictionary<string, HeaderStringValues>? headers = default)
+    HttpMethod httpMethod = default,
+    HttpContent content = default,
+    IDictionary<string, HeaderStringValues> headers = default)
     {
         httpMethod ??= HttpMethod.Get;
 
